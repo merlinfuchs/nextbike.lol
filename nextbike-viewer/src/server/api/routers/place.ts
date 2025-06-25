880336be80106929a2b2b49cbd2b43c43274b943
merlinfuchs <merlinfuchs2001@gmail.com>
2025-06-25 14:02:27 +0200

    filter out unused properties from places

--- nextbike-viewer/src/server/api/routers/place.ts
+++ nextbike-viewer/src/server/api/routers/place.ts
@@ -3,7 +3,15 @@ import { db } from "~/server/db";
 
 export const placeRouter = createTRPCRouter({
   getPlaces: publicProcedure.query(async () => {
-    const places = await db.collection("places").find({}).toArray();
+    const places = await db
+      .collection("places")
+      .find(
+        {},
+        {
+          projection: { _id: 1, lat: 1, lng: 1, spot: 1, bike: 1 },
+        },
+      )
+      .toArray();
     return places;
   }),
 });
